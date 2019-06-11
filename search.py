085760fe0bd6be705781600d7a6086998f8ee918
Merna177 <37479454+Merna177@users.noreply.github.com>
2019-06-11 22:41:16 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,41 +1,65 @@
+# how to test it ?
+#first input -> Enter --> file name  ex: input.txt
+#second input --> enter --> input string ex: engineering 
+
+# i think if we use KMP algorithm we can search for string in linear time but i found it useless because input string
+#max size is 100 char  so this algorithm has complexity O(100*n) so i think if we make it O(n)
+#wont make a different
+
+
+
 index = 0;
 remaining_string = ""
+#search : loop and compare each 2 character if they are similar then continue
+#not then we increment start pointer by one
+#complexity O(inputsize* file size) which is approx. O(n)
 def search(input_string,string_file):
-    window_size= len(input_string)
+    #length of input string
+    ln= len(input_string)
     start=0
     global index
     global  remaining_string
-
+   #if there is matching substring from the previous chunk so i added to new chunk to complete the comparsion
     index_of_string=index
     string_file = remaining_string + string_file
-    while index < window_size and index_of_string < len(string_file):
+    while index < ln and index_of_string < len(string_file):
+        #if equal continue
         if input_string[index] == string_file[index_of_string]:
             index+=1
 
             index_of_string+=1
         else:
+            # make index = 0 and start incremented by one
             index=0
             start+=1
             index_of_string=start
-        if(index == window_size):
+        if(index == ln):
             return True
     remaining_string = string_file[len(string_file)-index:]
 
     return False
 
-def read_chunck(filename):
+def read_chunck(filename,input_string):
+  flag=0
   f = open(filename,"r")
   while True:
-      s = f.read(3)
-      respone = search("rna",s)
+      # read 100 char only per iteration due to the large size of the file
+      s = f.read(100)
+      respone = search(input_string,s)
       if(respone == True):
           print("Found")
+          flag=1
           break
-      else:
-          print("NOT")
       if not s:
           break;
+  if flag ==0:
+      print("NOT FOUND")
 
 
 
-read_chunck("input.txt")
+
+
+if __name__ == '__main__':
+    file_name= input()
+    input_string =  input()
+    read_chunck(file_name,input_string)
